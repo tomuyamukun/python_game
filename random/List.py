3c69376247f62ab8c04ee4e6281be6a2f77b2320
tomuyamukun <3156tomochan@gmail.com>
2021-06-05 20:57:50 +0900

    main

---
+++ random/List.py
@@ -0,0 +1,9 @@
+states_of_america = ["Delaware", "Pennsylvania"]
+
+states_of_america[1] = "Pencilvania"
+
+states_of_america.append("American")
+
+states_of_america.extend(["angelaland", "Jack Bauer Land"])
+
+print(states_of_america)
---
+++ random/dirty_dozen.py
@@ -0,0 +1,6 @@
+fruits = ["Strawberries", "Nectarines", "Apples", "Grapes", "Peaches", "Pears"]
+vegetables = ["Spinach", "kale", "Tomatoes", "Celery", "Potatoes"]
+
+dirty_dozen = [fruits, vegetables]
+
+print(dirty_dozen)
