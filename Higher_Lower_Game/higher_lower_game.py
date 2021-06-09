b40e7970a2aa53794ed86a182526cacb3d8f8e4c
tomuyamukun <3156tomochan@gmail.com>
2021-06-09 18:46:54 +0900

    coffee

--- Higher_Lower_Game/higher_lower_game.py
+++ Higher_Lower_Game/higher_lower_game.py
@@ -6,54 +6,57 @@ from replit import clear
 
 # print文のデータをまとめている
 def format_data(data):
-  data_name = data["name"]
-  data_description = data['description']
-  data_country = data['country']
-  return f"{data_name}, a {data_description}, from {data_country}"
-
+    data_name = data["name"]
+    data_description = data['description']
+    data_country = data['country']
+    return f"{data_name}, a {data_description}, from {data_country}"
 
 
 def game():
 
-  continue_game = True
-  score = 0
-  while continue_game == True:
-    data_a = random.choice(data)
+    continue_game = True
+    score = 0
+
+# data_bが2回目以降そのままdata_aとなる
     data_b = random.choice(data)
-    print(logo)
 
-    if score > 0:
-      print(f"You're right! Current score: {score}.")
+    while continue_game:
 
-    print(f"Compare A: {format_data(data_a)}")
-    print(vs)
-    print(f"Against B: {format_data(data_b)}")
+        data_a = data_b
+        data_b = random.choice(data)
 
+        print(logo)
 
+        if score > 0:
+            print(f"You're right! Current score: {score}.")
 
+        print(f"Compare A: {format_data(data_a)}")
+        print(vs)
+        print(f"Against B: {format_data(data_b)}")
 
-    follower_a = data_a['follower_count']
+        follower_a = data_a['follower_count']
 
-    follower_b = data_b['follower_count']
+        follower_b = data_b['follower_count']
 
-    you_choice = input("Who has more followers? Type 'A' or 'B': ")
+        you_choice = input("Who has more followers? Type 'A' or 'B': ")
 
-    # 大文字小文字区別しないで判定
-    if you_choice.upper() == 'A':
-      if follower_a > follower_b:
-        score += 1
-        clear()
-      else:
-        print(f"Sorry, that's wrong. Final score: {score}")
-        continue_game = False
-    elif you_choice.upper() == 'B':
-        if follower_b > follower_a:
-          score += 1
-          clear()
+        # 大文字小文字区別しないで判定
+        if you_choice.upper() == 'A':
+            if follower_a > follower_b:
+                score += 1
+                clear()
+            else:
+                print(f"Sorry, that's wrong. Final score: {score}")
+                continue_game = False
+        elif you_choice.upper() == 'B':
+            if follower_b > follower_a:
+                score += 1
+                clear()
+            else:
+                print(f"Sorry, that's wrong. Final score: {score}")
+                continue_game = False
         else:
-          print(f"Sorry, that's wrong. Final score: {score}")
-          continue_game = False
-    else:
-      clear()
+            clear()
+
 
 game()
---
+++ coffee_machine/main.py
@@ -0,0 +1,90 @@
+MENU = {
+    "espresso": {
+        "ingredients": {
+            "water": 50,
+            "coffee": 18,
+        },
+        "cost": 1.5,
+    },
+    "latte": {
+        "ingredients": {
+            "water": 200,
+            "milk": 150,
+            "coffee": 24,
+        },
+        "cost": 2.5,
+    },
+    "cappuccino": {
+        "ingredients": {
+            "water": 250,
+            "milk": 100,
+            "coffee": 24,
+        },
+        "cost": 3.0,
+    }
+}
+
+profit = 0
+resources = {
+    'water': 300,
+    'milk': 200,
+    'coffee': 100,
+}
+
+
+def is_resource__sufficient(order_ingredients):
+    """Returns True when order can be made, False if ingredients are insufficient."""
+    for item in order_ingredients:
+        if order_ingredients[item] >= resources[item]:
+            print(f"Sorry there is not enough {item}.")
+            return False
+    return True
+
+
+def process_coins():
+    """ Returns True when order can be mode, False if ingredients are insufficient. """
+    print("Please insert coins")
+    total = int(input("how many quarters?: ")) * 0.25
+    total += int(input("how many dines?: ")) * 0.1
+    total += int(input("how manynickles?: ")) * 0.05
+    total += int(input("how many pennies?: ")) * 0.01
+    return total
+
+
+def is_transaction_successful(money_received, drink_cost):
+    """Return True when the payment is accepted, or False if money is insufficient. """
+    if money_received >= drink_cost:
+        change = round(money_received - drink_cost, 2)
+        print(f"Here is ${change} in change")
+        global profit
+        profit += drink_cost
+        return True
+    else:
+        print("sorry that' not enough money. Money refunded.")
+        return False
+
+
+def make_coffee(drink_name, order_ingredients):
+    """Deduct the required ingredients from resources."""
+    for item in order_ingredients:
+        resources[item] -= order_ingredients[item]
+    print(f"Here is your {drink_name}")
+
+
+is_on = True
+
+while True:
+    choice = input("​What would you like? (espresso/latte/cappuccino): ")
+    if choice == "off":
+        is_on = False
+    elif choice == "report":
+        print(f"water: {resources['water']}ml")
+        print(f"milk: {resources['milk']}ml")
+        print(f"coffee: {resources['coffee']}g")
+        print(f"money: {profit}")
+    else:
+        drink = MENU[choice]
+        if is_resource__sufficient(drink['ingredients']):
+            payment = process_coins()
+            if is_transaction_successful(payment, drink['cost']):
+                make_coffee(choice, drink['ingredients'])
