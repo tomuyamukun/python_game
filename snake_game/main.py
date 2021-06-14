403214918da0ee15040e75811473a438df6f270e
tomuyamukun <3156tomochan@gmail.com>
2021-06-14 16:31:10 +0900

    snakegame完成

---
+++ snake_game/main.py
@@ -0,0 +1,61 @@
+import time
+from turtle import Screen
+from snake import Snake
+from food import Food
+from scoreboard import Score
+
+
+screen = Screen()
+screen.setup(width=600, height=600)
+screen.bgcolor("black")
+screen.title("My Snake Game")
+screen.tracer(0)
+
+snake = Snake()
+food = Food()
+scoreboard = Score()
+
+
+screen.listen()
+screen.onkey(snake.up, "Up")
+screen.onkey(snake.down, "Down")
+screen.onkey(snake.left, "Left")
+screen.onkey(snake.right, "Right")
+
+
+game_is_on = True
+
+while game_is_on:
+    screen.update()
+    time.sleep(0.1)
+
+    snake.move()
+
+    # 食べ物との衝突
+    if snake.head.distance(food) < 15:
+        food.refresh()
+        snake.extend()
+        scoreboard.increase_score()
+
+    # 壁との衝突
+    if snake.head.xcor() > 280 or snake.head.xcor() < - \
+            280 or snake.head.ycor() > 280 or snake.head.ycor() < -280:
+        game_is_on = False
+        scoreboard.game_over()
+
+    # 頭がボディにぶつかったら終わり
+    # for segment in snake.segments:
+    #     if segment == snake.head:
+    #         pass
+    #     elif snake.head.distance(segment) < 10:
+    #         game_is_on = False
+    #         scoreboard.game_over()
+
+    # スライス使用上と同じ
+    for segment in snake.segments[1:]:
+        if snake.head.distance(segment) < 10:
+            game_is_on = False
+            scoreboard.game_over()
+
+
+screen.exitonclick()
---
+++ snake_game/scoreboard.py
@@ -0,0 +1,30 @@
+from turtle import Turtle
+FONT = ("Arial", 24, "normal")
+
+
+class Score(Turtle):
+
+    def __init__(self):
+        super().__init__()
+        self.score = 0
+        self.color("white")
+        self.penup()
+        self.goto(0, 270)
+        self.update_scoreboard()
+        self.hideturtle()
+
+    def update_scoreboard(self):
+        self.write(
+            f"Score: {self.score}",
+            align="center",
+            font=FONT
+        )
+
+    def increase_score(self):
+        self.score += 1
+        self.clear()
+        self.update_scoreboard()
+
+    def game_over(self):
+        self.goto(0, 0)
+        self.write("Game Over", align="center", font=FONT)
